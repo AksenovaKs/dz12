f0739dccb6c4842ae3d20b434c66e71da0bb393c
AksenovaKs <129744721+AksenovaKs@users.noreply.github.com>
2023-07-11 17:02:48 +0300

    Add files via upload

---
+++ дз 12.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+
+bool IsKPeriodic(const std::string& str, int K)
+{
+    if (K <= 0) {
+        return false; // Если K меньше или равно 0, строка не может быть кратной
+    }
+
+    int n = str.length();
+
+    if (n % K != 0) {
+        return false; // Если длина строки не кратна K, она не может быть кратной
+    }
+
+    for (int i = 0; i < n - K; i++) {
+        if (str[i] != str[i + K]) {
+            return false; // Если текущие символы не совпадают, строка не кратна
+        }
+    }
+
+    return true; // Если все проверки прошли успешно, строка является кратной
+}
+
+int main()
+{
+    std::string str;
+    int K;
+
+    std::cout << "Введите строку: ";
+    std::cin >> str;
+
+    std::cout << "Введите K: ";
+    std::cin >> K;
+
+    if (IsKPeriodic(str, K)) {
+        std::cout << "Строка является кратной числу " << K << std::endl;
+    }
+    else {
+        std::cout << "Строка не является кратной числу " << K << std::endl;
+    }
+
+    return 0;
+}
